6202cd2310ce3775aec8db6ed9ed9009c7ec1e45
rosonerri <gabrieldotajayi4real@gmail.com>
2025-01-01 16:21:31 +0100

    LP Adjusted

--- FrontEnd/src/Components/Pages/HomeScreen.tsx
+++ FrontEnd/src/Components/Pages/HomeScreen.tsx
@@ -8,10 +8,10 @@ const HomeScreen = () => {
       <div className="absolute top-0 left-0 w-full h-full bg-black opacity-80"></div>
       <div className="absolute top-0 left-0 w-full h-full flex items-center justify-center text-white">
         <div className="flex justify-center items-center flex-col">
-          <h1 className="xl:text-[80px] text-[50px] ml:text-[65px]">
+          <h1 className="xl:text-[80px] text-[30px] ml:text-[65px]">
             Human Stories & Ideas
           </h1>
-          <p className="font-[400] text-[30px]">
+          <p className="font-[400] text-[17px] md:text-[30px]">
             <TypeWriter
               options={{
                 strings: [
--- FrontEnd/src/Components/Static/Header.tsx
+++ FrontEnd/src/Components/Static/Header.tsx
@@ -1,13 +1,13 @@
 import img from "../../assets/star (2).png";
 import { FaArrowDown } from "react-icons/fa6";
 import { FiMenu } from "react-icons/fi";
-import {Link} from "react-router-dom"
+import { Link } from "react-router-dom";
 const Header = () => {
   return (
     <div className="w-full h-[60px] flex justify-center items-center bg-[#BC9A7F] ">
       <div className="w-[95%] h-full flex justify-between">
         <div className="w-[20%] h-full flex items-center">
-          <div className="flex w-[20%]">
+          <div className="flex w-[80%] md:w-[20%]">
             <img src={img} alt="" className="" />
             <p className="mt-4 font-[700] text-[22px] text-[white] ">Blog</p>
           </div>
