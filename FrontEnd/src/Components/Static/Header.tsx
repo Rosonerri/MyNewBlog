3008f2962b2fb1ec1b0d639c65a2a45fd8ce2854
rosonerri <gabrieldotajayi4real@gmail.com>
2025-01-03 08:07:33 +0100

    Text size adjusted

--- FrontEnd/src/Components/Static/Header.tsx
+++ FrontEnd/src/Components/Static/Header.tsx
@@ -4,7 +4,7 @@ import { FiMenu } from "react-icons/fi";
 import { Link } from "react-router-dom";
 const Header = () => {
   return (
-    <div className="w-full h-[60px] flex justify-center items-center bg-[#BC9A7F] ">
+    <div className="w-full h-[60px] flex justify-center items-center bg-[#BC9A7F] text-[12px]">
       <div className="w-[95%] h-full flex justify-between">
         <div className="w-[20%] h-full flex items-center">
           <div className="flex w-[80%] md:w-[20%]">
