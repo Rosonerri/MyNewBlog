014f424b4c474c97640220346ac9eaf2274a3c7e
rosonerri <gabrieldotajayi4real@gmail.com>
2025-01-10 18:04:47 +0100

    Updated Status in Model

--- FrontEnd/src/Components/Static/Header.tsx
+++ FrontEnd/src/Components/Static/Header.tsx
@@ -21,7 +21,7 @@ const Header = () => {
             </div>
           </div>
           <div className="flex justify-center items-center">
-            <div>Resources</div>
+            <div>Resource</div>
             <div>
               <FaArrowDown />
             </div>
