66a98d6a380ca537dd0f8e1251f8b2387a9b0765
rosonerri <gabrieldotajayi4real@gmail.com>
2025-02-14 21:44:59 +0100

    model

--- FrontEnd/src/Auth/RegisterCard.tsx
+++ FrontEnd/src/Auth/RegisterCard.tsx
@@ -19,7 +19,7 @@ const RegisterCard = () => {
         <p className="mt-1 w-[90%] text-[13px] font-[400]">
           You are seeing this message because you've created an account with us.
           Please go check your email, there you'd see a verification link and
-          your secret login token code
+          your secret login token
         </p>
       </div>
     </div>
