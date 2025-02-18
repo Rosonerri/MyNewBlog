201adcf85ea8756351e58cd3228e5f2135c1aa49
rosonerri <gabrieldotajayi4real@gmail.com>
2025-02-18 20:56:44 +0100

    All

--- FrontEnd/src/Auth/RegisterCard.tsx
+++ FrontEnd/src/Auth/RegisterCard.tsx
@@ -18,7 +18,7 @@ const RegisterCard = () => {
         <br />
         <p className="mt-1 w-[90%] text-[13px] font-[400]">
           You are seeing this message because you've created an account with us.
-          Please go check your email,
+          Please go check your
         </p>
       </div>
     </div>
