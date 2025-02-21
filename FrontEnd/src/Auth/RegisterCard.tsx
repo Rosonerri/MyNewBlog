51e4de44b85697ca937d5844a162d5e9ab7bfd68
rosonerri <gabrieldotajayi4real@gmail.com>
2025-02-21 20:41:52 +0100

    All

--- FrontEnd/src/Auth/RegisterCard.tsx
+++ FrontEnd/src/Auth/RegisterCard.tsx
@@ -17,7 +17,7 @@ const RegisterCard = () => {
         <div className="w-[97%] border-b " />
         <br />
         <p className="mt-1 w-[90%] text-[13px] font-[400]">
-          You are seeing this message because you've created an acoount wt
+          You are seeing this message because you've created an acooun
         </p>
       </div>
     </div>
