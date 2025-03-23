139c1899babf7bdbd909631e43dda4f8c12056da
rosonerri <gabrieldotajayi4real@gmail.com>
2025-03-23 20:07:36 +0100

    All

--- FrontEnd/src/Auth/RegisterCard.tsx
+++ FrontEnd/src/Auth/RegisterCard.tsx
@@ -17,7 +17,7 @@ const RegisterCard = () => {
         <div className="w-[97%] border-b " />
         <br />
         <p className="mt-1 w-[90%] text-[13px] font-[400]">
-          You are seiing this message because ypu just signed up for our servi
+          You are seiing this message because ypu just signed up for our s
         </p>
       </div>
     </div>
