6617c7c0b9954804aea96db3c57e632f2a668ec9
rosonerri <gabrieldotajayi4real@gmail.com>
2025-03-25 16:14:50 +0100

    all

--- FrontEnd/src/Auth/RegisterCard.tsx
+++ FrontEnd/src/Auth/RegisterCard.tsx
@@ -18,6 +18,7 @@ const RegisterCard = () => {
         <br />
         <p className="mt-1 w-[90%] text-[13px] font-[400]">
           You are seiing this message because you just signed up for our
+          Services
         </p>
       </div>
     </div>
