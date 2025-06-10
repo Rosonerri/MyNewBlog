fa04f423fda5e5755dfab8eaf8760f4a8302171d
Rosonerri <gabrieldotajayi4real@gmail.com>
2025-06-10 10:57:35 +0100

    All

--- FrontEnd/src/Auth/RegisterCard.tsx
+++ FrontEnd/src/Auth/RegisterCard.tsx
@@ -17,7 +17,7 @@ const RegisterCard = () => {
         <div className="w-[97%] border-b " />
         <br />
         <p className="mt-1 w-[90%] text-[13px] font-[400]">
-          You are seeing this message because you jus
+          You are seeing this message because
         </p>
       </div>
     </div>
