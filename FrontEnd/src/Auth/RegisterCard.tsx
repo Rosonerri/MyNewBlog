7ba4fc62faabd74996f4398aaef8793aaa3f268e
Rosonerri <gabrieldotajayi4real@gmail.com>
2025-06-10 10:59:41 +0100

    All

--- FrontEnd/src/Auth/RegisterCard.tsx
+++ FrontEnd/src/Auth/RegisterCard.tsx
@@ -16,9 +16,7 @@ const RegisterCard = () => {
         <br />
         <div className="w-[97%] border-b " />
         <br />
-        <p className="mt-1 w-[90%] text-[13px] font-[400]">
-          You are seeing this m
-        </p>
+        <p className="mt-1 w-[90%] text-[13px] font-[400]">You are seeings</p>
       </div>
     </div>
   );
