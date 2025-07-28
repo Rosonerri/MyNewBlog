cf1647e8068dc90a2ed5d2cc362de3e91cf69c42
Rosonerri <gabrieldotajayi4real@gmail.com>
2025-07-28 20:24:59 +0100

    A

--- FrontEnd/src/Auth/RegisterCard.tsx
+++ FrontEnd/src/Auth/RegisterCard.tsx
@@ -18,11 +18,9 @@ const RegisterCard = () => {
         <br />
         <p className="mt-1 w-[90%] text-[13px] font-[400]">
           You are seeing this message because you just created an account with
-          us
         </p>
       </div>
     </div>
   );
 };
-
 export default RegisterCard;
