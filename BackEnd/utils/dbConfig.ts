1c51e8ef661c81f2bf4910c72326758a8c23da14
Rosonerri <gabrieldotajayi4real@gmail.com>
2025-09-02 22:42:02 +0100

    All

--- BackEnd/utils/dbConfig.ts
+++ BackEnd/utils/dbConfig.ts
@@ -1,13 +1,13 @@
-import { connect } from "mongoose";
+// import { connect } from "mongoose";
 
-const URL: string = "mongodb://127.0.0.1:27017/myNewBlog";
+// const URL: string = "mongodb://127.0.0.1:27017/myNewBlog";
 
-export const dbConfig = async () => {
-  try {
-    return await connect(URL).then(() => {
-      console.log("DataBase Connected Successfully🌎🌎");
-    });
-  } catch (error) {
-    return error;
-  }
-};
+// export const dbConfig = async () => {
+//   try {
+//     return await connect(URL).then(() => {
+//       console.log("DataBase Connected Successfully🌎🌎");
+//     });
+//   } catch (error) {
+//     return error;
+//   }
+// };
