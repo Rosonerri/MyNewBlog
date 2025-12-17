7417a14d2972342e856a846df8c02c5da980865c
Rosonerri <gabrieldotajayi4real@gmail.com>
2025-12-17 01:43:55 +0100

    All

--- FrontEnd/src/Auth/RegisterCard.tsx
+++ FrontEnd/src/Auth/RegisterCard.tsx
@@ -17,7 +17,7 @@ const RegisterCard = () => {
         <div className="w-[97%] border-b " />
         <br />
         <p className="mt-1 w-[90%] text-[13px] font-[400]">
-          You are seeing this message this message because
+          You are seeing this message this message be
         </p>
       </div>
     </div>
